08f2df91dde100c3619c93cde60edd3b28da411e
Tesohh <simozilla006@gmail.com>
2022-02-07 21:30:18 +0100

    personaggio gordo

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -174,6 +174,8 @@ func _ready():
 
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func shoot():
 #	var t = $GunRecoil
@@ -216,10 +218,12 @@ func _physics_process(delta):
 		input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 		input_vector = input_vector.normalized()
 		if input_vector != Vector2.ZERO:
-			animationPlayer.play("WalkForward", -1, 0.5)
+			animationTree.set("parameters/Idle/blend_position", input_vector)
+			animationTree.set("parameters/Walk/blend_position", input_vector)
+			animationState.travel("Walk")
 			velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 		else:
-			animationPlayer.play("RESET")
+			animationState.travel("Idle")
 			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 		
 		velocity = move_and_slide(velocity)
